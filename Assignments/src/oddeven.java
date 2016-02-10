e5dad74d68cd1348e3d1b80ec97f4b0b9f5e92c1
Pradeep Rajula <pradeep.rajula@gmail.com>
2016-02-09 20:07:38 -0600

    updated person

--- Assignments/src/oddeven.java
+++ Assignments/src/oddeven.java
@@ -1,34 +1,23 @@
-import java.util.Scanner;
 
-public class oddeven {
-
-		
-		String result;
-		oddeven(int number){
-			if(number % 2 == 0){
-				result = "Even";
-			}else{
-				result = "Odd";
-			}
-		}
-		void display(){
-			System.out.println(result);
-		}
-		
-		public static void main(String[] args) {
-			// TODO Auto-generated method stub
-			Scanner keyboard = new Scanner(System.in);
-		    System.out.print("Please enter number1: ");
-		    int n1 = keyboard.nextInt();  // local variable
-		    System.out.print("Please enter vumber2: " );
-		    int n2 = keyboard.nextInt();  // another local variable
-		    keyboard.nextLine();  // to handle the end of line characters
-		    oddeven s1 = new oddeven(n1);  
-		    oddeven s2 = new oddeven(n2);  
-		    s1.display();  
-		    s2.display(); 
-		    
-			}
-		
-
-}
+class oddeven{  
+    int id;  
+    String name;  
+      
+    oddeven(int i){  
+    id = i;  
+    if(id % 2 == 0){
+    	name = "even";
+    }else{
+    	name = "odd";
+    }
+    	
+    }  
+    void display(){System.out.println(name);}  
+   
+    public static void main(String args[]){  
+    oddeven s1 = new oddeven(111);  
+    oddeven s2 = new oddeven(222);  
+    s1.display();  
+    s2.display();  
+   }  
+}  
